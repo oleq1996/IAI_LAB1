e8d20d2f5adcfe28310a5cb6c944dd5e5a0b4133
p0lny <okucharzyk.96@gmail.com>
2019-10-18 09:58:21 +0200

    Zmiana adresow dostepu

--- js/navbar.js
+++ js/navbar.js
@@ -1,12 +1,12 @@
 let nav = '<ul class="nav justify-content-center">'+
     '<li class="nav-item">'+
-    '<a class="nav-link" href="index.html">Home</a></li>'+
+    '<a class="nav-link" href="../html/index.html">Home</a></li>'+
 '<li class="nav-item">'+
    ' <a class="nav-link" href="">Blog</a></li>'+
 '<li class="nav-item">'+
    ' <a class="nav-link" href="">Quiz</a></li>'+
 '<li class="nav-item">'+
-    '<a class="nav-link" href="contact.html">Kontakt</a></li>'+
+    '<a class="nav-link" href="../html/contact.html">Kontakt</a></li>'+
 '</ul>';
 
 let elem = document.querySelector('.navbar2');
---
+++ js/script.js
@@ -0,0 +1,10 @@
+for(let i=1;i<=10;i++){
+   console.log('\n');
+    for(let j=1;j<=10;j++)
+    {
+        let wynik;
+        wynik=i*j;
+        console.log(wynik);
+
+    }
+}
