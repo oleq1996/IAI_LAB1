342fab425e9210cbedb484a6c19e04c7c362a77b
p0lny <okucharzyk.96@gmail.com>
2019-10-18 09:21:49 +0200

    dodany skrypt JS do obslugi navbaru

---
+++ navbar.js
@@ -0,0 +1,13 @@
+let nav = '<ul class="nav justify-content-center">'+
+    '<li class="nav-item">'+
+    '<a class="nav-link" href="index.html">Home</a></li>'+
+'<li class="nav-item">'+
+   ' <a class="nav-link" href="">Blog</a></li>'+
+'<li class="nav-item">'+
+   ' <a class="nav-link" href="">Quiz</a></li>'+
+'<li class="nav-item">'+
+    '<a class="nav-link" href="contact.html">Kontakt</a></li>'+
+'</ul>';
+
+let elem = document.querySelector('.navbar2');
+elem.innerHTML=nav;
